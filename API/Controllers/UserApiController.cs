00456532bf2673d5080db11ff89150e2a52e533f
Nishit Vataliya <nishitvataliya003@gmail.com>
2025-02-17 10:36:56 +0530

    Authorization Usign JWT

--- API/Controllers/UserApiController.cs
+++ API/Controllers/UserApiController.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Repositories;
 
 namespace MyApp.Namespace
@@ -9,10 +13,12 @@ namespace MyApp.Namespace
     public class UserApiController : ControllerBase
     {
         private readonly IUserInterface _userRepo;
+        private readonly IConfiguration myConfig;
 
-        public UserApiController(IUserInterface userRepo)
+        public UserApiController(IUserInterface userRepo, IConfiguration config)
         {
             _userRepo = userRepo;
+            myConfig = config;
         }
 
         [HttpPost]
@@ -53,7 +59,7 @@ namespace MyApp.Namespace
 
             }
         }
-        
+
         [HttpPost]
         [Route("Login")]
         public async Task<IActionResult> Login([FromForm] vm_Login user)
@@ -61,11 +67,27 @@ namespace MyApp.Namespace
             t_User UserData = await _userRepo.Login(user);
             if (UserData.c_UserId != 0)
             {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim("Userid", UserData.c_UserId.ToString()),
+                    new Claim("UserName", UserData.c_UserName)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(myConfig["Jwt:Key"]));
+                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                issuer: myConfig["Jwt:Issuer"],
+                audience: myConfig["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: signIn
+                );
                 return Ok(new
                 {
                     success = true,
                     message = "Login Success",
-                    UserData = UserData
+                    UserData = UserData,
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
                 });
             }
             else
