d3c44628653475fd68184a7a4c2e93d65ee047ed
Nishit Vataliya <nishitvataliya003@gmail.com>
2025-02-18 22:34:01 +0530

    TokenList update

--- MVC/Controllers/ContactController.cs
+++ MVC/Controllers/ContactController.cs
@@ -30,6 +30,10 @@ namespace MVC.Controllers
         {
             return View();
         }
+        public IActionResult _TokenList()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
